b9a65df3b7a134af6712f471d30e7a6e11fc3ecb
Setareh Khoylow <setarehkhoylow@Setarehs-MacBook-Pro.local>
2023-10-24 13:44:00 -0700

    Added credit for the background photo in the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [currentForm, setCurrentFrom] = useState('login')
 
   const myStyle={
-    backgroundImage: "url(" + background + ")",
+    backgroundImage: "url(" + background + ")", // <a href="https://www.freepik.com/free-vector/ai-technology-brain-background-vector-digital-transformation-concept_16268324.htm#query=digital%20brain&position=33&from_view=search&track=ais">Image by rawpixel.com</a> on Freepik
     width: '100vw',
     height: '100vh',
     backgroundSize: 'cover',
