fb63b73c1bb1c3b1eec8fc05c0719f34c3d57e50
TanishaAAgarwal4 <taagarwa@uci.edu>
2023-10-30 22:01:16 -0700

    add participant

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from routes.assessment import assessment_router
 from routes.auth import auth_router
 from routes.survey import survey_router
+from routes.participant import participant_router
 from settings import settings
 
 app = FastAPI()
@@ -22,6 +23,7 @@ async def shutdown_db_client():
 
 
 app.include_router(survey_router, tags=["surveys"], prefix="/survey")
+app.include_router(participant_router, tags=["participant"], prefix="/participant")
 app.include_router(auth_router, tags=["auth"], prefix="")
 app.include_router(assessment_router, tags=["assessments"], prefix="/assessment")
 
---
+++ backend/models/participant.py
@@ -0,0 +1,13 @@
+import uuid
+
+from pydantic import BaseModel, Field, EmailStr
+
+
+class Participant(BaseModel):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")  # how to validate unique ID
+    first_name: str = Field(...)
+    last_name: str = Field(...)
+    email: EmailStr = Field(...)
+    assessment_ids: list[str] = Field(...)
+
+
