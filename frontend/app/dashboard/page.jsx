15fd7a01413ac6edbe22bb4089efdbe2e5e6cb33
Setareh Khoylow <setarehkhoylow@Setarehs-MacBook-Pro.local>
2023-11-02 13:04:39 -0700

    Cleaned up commented out code

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -115,43 +115,4 @@ const Dashboard = (props) => {
     )
 }
 
-export default Dashboard
-
-                    // {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-                    // <div className="md:hidden" id="mobile-menu">
-                    //     <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                    //         {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                    //         <Link href="/dashboard" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</Link>
-                    //         <Link href="/assessments" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Assessments</Link>
-                    //         <Link href="/participants" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Participants</Link>
-                    //     </div>
-                    //     <div className="border-t border-gray-700 pb-3 pt-4">
-                    //         <div className="flex items-center px-5">
-                    //             <div className="flex-shrink-0">
-                    //                 <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                    //             </div>
-                    //             <div className="ml-3">
-                    //                 <div className="text-base font-medium leading-none text-white">Tom Cook</div>
-                    //                 <div className="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
-                    //             </div>
-                    //         </div>
-                    //         <div className="mt-3 space-y-1 px-2">
-                    //             <a href="#" onClick={handleLogout} className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</a>
-                    //         </div>
-                    //     </div>
-                    // </div>
-                    // <div className="-mr-2 flex md:hidden">
-                    //             {/* <!-- Mobile menu button --> */}
-                    //             <button type="button" className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
-                    //                 <span className="absolute -inset-0.5"></span>
-                    //                 <span className="sr-only">Open main menu</span>
-                    //                 {/* <!-- Menu open: "hidden", Menu closed: "block" --> */}
-                    //                 <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                    //                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    //                 </svg>
-                    //                 {/* <!-- Menu open: "block", Menu closed: "hidden" --> */}
-                    //                 <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                    //                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    //                 </svg>
-                    //             </button>
-                    //         </div>
+export default Dashboard;
