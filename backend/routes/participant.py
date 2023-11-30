6b0f2e431a488b2fd9391e9db514e22f053af478
TanishaAAgarwal4 <taagarwa@uci.edu>
2023-11-29 16:15:38 -0800

    get participants api with all the assessments for particular researcher

--- backend/routes/participant.py
+++ backend/routes/participant.py
@@ -28,16 +28,20 @@ async def create_participant(request: Request, participant: Participant = Body(.
       return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_participant)
         
     
-@participant_router.get("/", response_description="Get all Participants and their Assessment names")
-async def list_participants(request: Request):
+@participant_router.get("/{id}", response_description="Get all Participants and their Assessment names")
+async def list_participants(id: str, request: Request):
     participants = []
     participant_list = await request.app.mongodb["Participant"].find().to_list(length=None)
     for participant in participant_list:
       assessment_obj=[]
       for assessment in participant.get('assessment_ids') :
          if(await request.app.mongodb["Assessment"].find_one({"_id": assessment})) is not None:
-            assessment_obj.append(await request.app.mongodb["Assessment"].find_one({"_id": assessment}))
-      participant["assessment_ids"] = assessment_obj  
-      participants.append(participant)
+            assessmentForParticularParticipant = await request.app.mongodb["Assessment"].find_one({"_id": assessment})
+            if(assessmentForParticularParticipant.get("researcher_id") == id) :
+               assessment_obj.append(await request.app.mongodb["Assessment"].find_one({"_id": assessment}))
+
+      if len(assessment_obj) != 0:
+         participant["assessment_ids"] = assessment_obj  
+         participants.append(participant)
     return participants
   
