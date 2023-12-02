df3823d1fe62c4f193ea9444bcc88511c3886215
Setareh Khoylow <setarehkhoylow@Setarehs-MacBook-Pro.local>
2023-12-01 19:24:39 -0800

    Fixed logo on login page

--- frontend/app/auth/login/page.jsx
+++ frontend/app/auth/login/page.jsx
@@ -79,49 +79,45 @@ export const Login = (props) => {
   
   };
 
-  const myStyle = {
-    backgroundImage: "/images/background.jpg", // <a href="https://www.freepik.com/free-vector/ai-technology-brain-background-vector-digital-transformation-concept_16268324.htm#query=digital%20brain&position=33&from_view=search&track=ais">Image by rawpixel.com</a> on Freepik
-  };
-
   return (
-    <div className="hero min-h-screen bg-base-200">
-        <img className="lg:w-1/2 w-2/3 flex self-start p-5 mt-8" src="/images/digital-neurology-logo-dark.png" alt="Digital Neurology" />
-      <div className="hero-content flex-col lg:flex-row max-w-4xl mt-5">
-        <div className="text-center lg:text-left">
-            <h1 className="text-6xl lg:text-5xl font-bold text-stone-700">Welcome</h1>
-            <p className="py-6 text-stone-500 lg:text-sm text-lg font-bold">This platform allows researchers with limited technical skills to be able to create assessments or surveys easily and efficiently so they can collect patient data.</p>
+    <div className="hero min-h-screen bg-base-200 flex flex-col">
+        <img className="hero-content mt-8 lg:w-1/2 w-2/3 flex self-center" src="/images/digital-neurology-logo-dark.png" alt="Digital Neurology" />
+        <div className="hero-content flex-col lg:flex-row max-w-4xl">
+            <div className="text-center lg:text-left">
+                <h1 className="text-6xl lg:text-5xl font-bold text-stone-700">Welcome</h1>
+                <p className="py-6 text-stone-500 lg:text-sm text-lg font-bold">This platform allows researchers with limited technical skills to be able to create assessments or surveys easily and efficiently so they can collect patient data.</p>
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-2xl shadow-2xl bg-base-100 lg:max-w-xl">
+                <form className="px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                        Email
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email"/>
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+                        {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
+                    </div>
+                    <div className="flex justify-center">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
+                        Log In
+                        </button>
+                    </div>
+                    <div className="flex items-center justify-between my-3">
+                        <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/auth/register">
+                        Don't Have An Account?
+                        </Link>
+                        <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/auth/forgot">
+                        Forgot Password?
+                        </Link>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div className="card flex-shrink-0 w-full max-w-2xl shadow-2xl bg-base-100 lg:max-w-xl">
-          <form className="px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-              <div className="mb-4">
-                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                  Email
-                  </label>
-                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email"/>
-              </div>
-              <div className="mb-6">
-                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                  Password
-                  </label>
-                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-                  {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
-              </div>
-              <div className="flex justify-center">
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
-                  Log In
-                  </button>
-              </div>
-              <div className="flex items-center justify-between my-3">
-                  <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/auth/register">
-                  Don't Have An Account?
-                  </Link>
-                  <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/auth/forgot">
-                  Forgot Password?
-                  </Link>
-              </div>
-          </form>
-        </div>
-      </div>
     </div> 
   );
 };
